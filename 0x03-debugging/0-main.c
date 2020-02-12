6554b18b79047af33c89f5014104bf99467d04f9
Andrew C <a.cox@me.com>
2020-02-12 05:33:26 +0000

    Test file for positive or negative function

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * main - tests positive_or_negative function
+ * 
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	num = 0;
+	positive_or_negative(num);
+
+	return (0);
+}
