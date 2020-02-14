98bb2cfb52417dfb42d9e9091c13b83c4798684e
Andrew C <a.cox@me.com>
2020-02-14 06:59:44 +0000

    Add 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *mul - multiply 2 input numbers
+ *
+ * @a: first number
+ *
+ * @b: second number
+ *
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
