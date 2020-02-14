b01ce219d0f1a27a059dac1814ce5b927847f0a4
Andrew C <a.cox@me.com>
2020-02-14 07:02:20 +0000

    Add 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - print numbers 0-9
+ *
+ *Return: Void
+ */
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num = '0'; num < '10'; num++)
+		_putchar (i);
+	_putchar ('\n');
+}
