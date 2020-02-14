ed9510d0ffd467a8ab37f4313b2cb1b8436e77d9
Andrew C <a.cox@me.com>
2020-02-14 06:51:08 +0000

    Add 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _isupper - check if letter is upper or lower
+ *
+ * @c: letter to be checked
+ * 
+ * Return: 1 if upper 0 if not
+ */
+int _isupper(int c)
+{
+
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
