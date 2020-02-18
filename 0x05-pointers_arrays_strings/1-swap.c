b8739545435e7d0b03fbdecb888d0ca7f93d00f7
Andrew C <a.cox@me.com>
2020-02-18 19:03:43 +0000

    Swaps value of two ints

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * swap_int - swaps value of 2 ints
+ *
+ * @a: pointer to int
+ *
+ * @b: pointer to int
+ *
+ * Return: Void
+ */
+void swap_int(int *a, int *b)
+{
+	int buffer = 0;
+
+	*a = buffer;
+	*a = *b;
+	*b = buffer;
+}
