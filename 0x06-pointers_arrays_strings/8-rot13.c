a718ee49dd4d06abd2dcc88452cc6e6d04e3fe04
Andrew C <a.cox@me.com>
2020-02-21 07:28:14 +0000

    Encodes in rot13

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * rot13 - encodes string using rot13
+ *
+ * @s: string to be encoded
+ * 
+ * Return: coded string
+ */
+
+char *rot13(char *s)
+{
+	int i, x;
+	char message[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char code[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+		for (x = 0; message[x] != '\0'; x++)
+		{
+			if (s[i] == message[x])
+			{
+				s[i] = code[x];
+				break;
+			}
+		}
+	return (s);
+}
