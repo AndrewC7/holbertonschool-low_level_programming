9b494f042f42603b7ddb794ab4dded55e59d8654
Andrew C <a.cox@me.com>
2020-02-25 02:23:59 +0000

    Add 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _strchr - locates character in string
+ *
+ * @s: string to look through
+ * @c: character to look for
+ *
+ * Return: first occurance of character or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	while(s++)
+	{
+		if (*s == c)
+			return (s);
+
+		if (*s == '\0')
+			return ('\0');
+	}
+	return ('\0');
+}
