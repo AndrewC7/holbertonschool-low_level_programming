1aeace945600c9575a645e96c9a64325c9e5e8cc
Andrew C <a.cox@me.com>
2020-02-26 21:53:56 +0000

    Rewrite/remove static int variable

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,12 +9,10 @@
  */
 int _strlen_recursion(char *s)
 {
-	static int count;
-
 	if (*s)
 	{
-		count++;
-		_strlen_recursion(s + 1);
+		return (_strlen_recursion(++s) + 1);
 	}
-	return (count);
+	else
+		return (0);
 }
