814a72d78a3c2a849731143d982a3d7dfbee0272
Andrew C <a.cox@me.com>
2020-03-02 03:08:25 +0000

    Add 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: empty
+ * @argv: array of strings passed to program
+ *
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
