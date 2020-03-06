07bd11c4121df5f1ca0ddf7ace76682942d69615
Andrew C <a.cox@me.com>
2020-03-06 21:57:42 +0000

    Add 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers of base 16
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		putchar(i);
+	for (i = 97; i <= 102; i++)
+		putchar(i);
+
+	putchar('\n');
+	return (0);
+}
