cc0dedebbdc71304e13bc153df7f8333820a9f0a
Andrew C <a.cox@me.com>
2020-03-06 22:14:05 +0000

    Add 10-print_comb2.c

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Print 0 to 99
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = '0'; num1 <= '9'; num1++)
+	{
+		for (num2 = '0'; num2 <= '9'; num2++)
+		{
+			putchar(num1);
+			putchar(num2);
+			if (num1 != '9' || num2 != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
