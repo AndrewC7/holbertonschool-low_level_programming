29ab68523367da5457b9842dda31b38513f6df26
Andrew C <a.cox@me.com>
2020-03-06 22:33:49 +0000

    Add 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - prints diagonal line on terminal
+ *
+ * @n: line length
+ *
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int x, y;
+
+	if (n <= 0)
+		_putchar ('\n');
+	for (x = 0; x < n; x++)
+	{
+		for (y = 0; y < x; y++)
+			_putchar(' ');
+		_putchar('\\');
+		_putchar ('\n');
+	}
+}
