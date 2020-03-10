abaf8d988858243836dd3b60fd1a5cd49b7f32b9
Andrew C <a.cox@me.com>
2020-03-10 03:03:03 +0000

    Add 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of program this file was compiled from
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
