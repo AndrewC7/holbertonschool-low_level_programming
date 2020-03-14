81bb32a28ba417d37d77418e8061eebfb3546594
AndrewC7 <a.cox@me.com>
2020-03-14 17:11:21 +0000

    Add 1-print_numbers.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers passed
+ *
+ * @separator: string of spaces
+ *
+ * @n: length of args being passed
+ *
+ * Return: Void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int counter;
+
+	va_list args;
+	va_start(args, n);
+
+
+	for (counter = 0; counter < n; counter++)
+	{
+		printf("%d", va_arg(args, const unsigned int));
+
+		if (separator != NULL && counter != (n - 1))
+			printf("%s", separator);
+	}
+	printf("\n");
+
+
+	va_end(args);
+}
