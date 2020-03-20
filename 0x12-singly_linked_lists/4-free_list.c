b4f4791b70905a7af8bb4b438368ff6d7b55a589
AndrewC7 <a.cox@me.com>
2020-03-20 04:32:11 +0000

    Add 4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_list - free list
+ *
+ * @head: start or list
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+}
