4f35e7c1d95e5b745ed8487a37f0e44dcd24f237
AndrewC7 <a.cox@me.com>
2020-03-30 16:51:59 +0000

    Add 8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of array
+ *
+ * @a: parameter passed
+ * @n: parameter passed
+ *
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int x;
+
+	for (x = 0; x < n; x++)
+		if (x < n - 1)
+			printf("%d, ", a[x]);
+		else
+			printf("%d", a[x]);
+
+	printf("\n");
+}
