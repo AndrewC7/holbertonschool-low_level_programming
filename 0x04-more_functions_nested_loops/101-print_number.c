50b37c892ed3b1e4a91d60f7daf47829c394dffa
AndrewC7 <a.cox@me.com>
2020-03-30 16:35:26 +0000

    Add 101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * print_number - converts int to string
+ * 
+ * @n: int passed
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	unsigned int y;
+
+	if (n > 9)
+	{
+		print_number(n / 10);
+		_putchar('0' + (n % 10));
+	}
+
+	else if (n > -1)
+		_putchar('0' + n);
+
+	else if (n > -10)
+	{
+		_putchar('-');
+		n = n * (-1);
+		_putchar('0' + n);
+	}
+	else
+	{
+		_putchar('-');
+		n = n * (-1);
+		y = (unsigned int) n;
+		print_number(y / 10);
+		_putchar('0' + (y % 10));
+	}
+}
